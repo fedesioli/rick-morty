24b9fdf7f14a7363a008b40efa0381ec2824d517
Federico Sioli <fsioli@udesa.edu.ar>
2021-04-09 11:55:45 -0300

    eventos

--- src/components/body.js
+++ src/components/body.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Tarjeta from './tarjetas';
 import arrayDePersonajes from '../rickandmorty.json';
+import { render } from '@testing-library/react';
 
-function Body(){
-    return(
-        <div className='personajesPadre'>    
-            {
-            arrayDePersonajes.map(function(personaje, idx){
-              return(
-            <div  key={idx} className="tarjetaPadre">
-                <Tarjeta datosPersonaje = {personaje} />
-            </div>    
-              )  
-                })
-            }   
-            
-        </div>   
-    )
+class Body extends Component{
+
+
+    Borrar = () => {arrayDePersonajes.filter(function(personaje){
+        return personaje = null
+    })}
+    Reset = () => {arrayDePersonajes = arrayDePersonajes}
+    
+    render(){
+
+        return(
+            <div className='personajesPadre'>    
+                {
+                arrayDePersonajes.map(function(personaje, idx){
+                  return(
+                
+                    <Tarjeta datosPersonaje = {personaje} key={idx} color="white" borrarPersonaje = {borrar}/>
+                    
+                  )  
+                    })
+                }   
+            <button onClick={this.Reset.bind(this)}>Resetear Personajes</button>
+            </div>   
+        )
+    }
 }
 
 export default Body; 
--- src/components/tarjetas.js
+++ src/components/tarjetas.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-function Tarjeta(props){
-    return(
-    <>
-        <img src={props.datosPersonaje.image} className='imagenTarjeta'></img>
-        <div className='tarjetaHijo'>
-        <h3>{props.datosPersonaje.name}</h3>
-        <p>{props.datosPersonaje.status} - {props.datosPersonaje.species}</p>
-        <p>Origin: {props.datosPersonaje.origin.name}</p>
+class Tarjeta extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            colorOriginal: props.color,              
+            color: "white",
+        }
+    }
+
+    cambiarColorHover(){
+        this.setState({
+            color: "lightgray",
+        })
+    }
+    cambiarColorHover2(){
+        this.setState({
+            color: "white",
+            
+        })
+    }
+    cambiarColorBoton(){
+        if(this.state.color === "white")
+            this.setState({
+                color: "pink",
+            })
+        else   
+            this.setState({
+                color: "white",
+            }) 
+    }   
+
+
+    render(){
+        return(
+        <div className="tarjetaPadre"  style = {{backgroundColor: this.state.color}}
+        onMouseEnter={this.cambiarColorHover.bind(this)}
+        onMouseOut={this.cambiarColorHover2.bind(this)}
+        >
+            <img src={this.props.datosPersonaje.image} className='imagenTarjeta'></img>
+            <div className='tarjetaHijo'>
+            <h3>{this.props.datosPersonaje.name}</h3>
+            <p>{this.props.datosPersonaje.status} - {this.props.datosPersonaje.species}</p>
+            <p>Origin: {this.props.datosPersonaje.origin.name}</p>
+            <button onClick={this.cambiarColorBoton.bind(this)}>Cambiar color</button>
+            <button onClick={this.props.borrarPersonaje.bind(this)}>Borrar</button>
+            </div>
         </div>
-    </>
-    );
+        );
+    }
 
 }
 
